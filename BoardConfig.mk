01ee2c99fe97979c46e5a62e13a630a07d2c056d
Batuhan Türkoğlu <76158371+batuhantrkgl@users.noreply.github.com>
2022-12-21 17:58:31 +0300

    j7y17lte: fix brightness path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -115,4 +115,4 @@ BOARD_RECOVERYIMAGE_PARTITION_SIZE := 39845888
 ######## PBRP FLAGS ########
 ############################
 
-PB_TORCH_PATH := "sys/devices/virtual/camera/flash/rear_flash"
+PB_TORCH_PATH := "/sys/devices/virtual/camera/flash/rear_flash"
