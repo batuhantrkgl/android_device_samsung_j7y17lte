03cb19bd6a584971cd75edb7a0867546cfe19146
Flominator <github.flominatorgd@gmail.com>
2023-02-05 03:51:25 +0100

    j7y17lte: bt drop cherokee

--- system_prop.mk
+++ system_prop.mk
@@ -2,9 +2,6 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=420
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.qcom.bluetooth.soc=cherokee
-
 # Fix for echo in calls
 PRODUCT_PROPERTY_OVERRIDES += \
     audio_hal.disable_two_mic=false
