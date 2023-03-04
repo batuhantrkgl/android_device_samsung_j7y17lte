d95703ea7b93f3e788e7ca4fa699051babfe5c72
Batuhan Türkoğlu <76158371+batuhantrkgl@users.noreply.github.com>
2023-03-04 23:00:06 +0300

    j7y17lte: change product_name to havoc

--- havoc_j7y17lte.mk
+++ havoc_j7y17lte.mk
@@ -27,7 +27,7 @@ $(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := j7y17lte
-PRODUCT_NAME := lineage_j7y17lte
+PRODUCT_NAME := havoc_j7y17lte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := Galaxy J7 2017
 PRODUCT_MANUFACTURER := samsung
