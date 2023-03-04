samsungexynos7870/android_device_samsung_a3y17lte

declare use of oss audio and uprev to android.hardware.soundtrigger@2.3-impl, android.hardware.audio.effect@6.0-impl, android.hardware.audio@6.0-impl, add device speciffic wifi overlay, add fix for hotspot on android 11 for qcom wifi by setting WIFI_AVOID_IFACE_RESET_MAC_CHANGE := true, add missing android.hardware.nfc.uicc.xml, Add parameters for Hotspot 2.0 in wpa_supplicant_overlay.conf, refractor for arrow

AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2019 The havocOS Project
+# Copyright (C) 2019 The arrowOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/havoc_a3y17lte.mk
+    $(LOCAL_DIR)/arrow_a3y17lte.mk
 
 COMMON_LUNCH_CHOICES := \
-    havoc_a3y17lte-user \
-    havoc_a3y17lte-userdebug \
-    havoc_a3y17lte-eng
+    arrow_a3y17lte-user \
+    arrow_a3y17lte-userdebug \
+    arrow_a3y17lte-eng

batuhantrkgl/android_device_samsung_j7y17lte

AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2019 The havocOS Project
+# Copyright (C) 2019 The arrowOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/havoc_j7y17lte.mk
+    $(LOCAL_DIR)/arrow_j7y17lte.mk
 
 COMMON_LUNCH_CHOICES := \
-    havoc_j7y17lte-user \
-    havoc_j7y17lte-userdebug \
-    havoc_j7y17lte-eng
+    arrow_j7y17lte-user \
+    arrow_j7y17lte-userdebug \
+    arrow_j7y17lte-eng