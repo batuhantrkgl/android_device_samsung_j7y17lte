02e58fcd91ad3a27a060548cb72ec618a7983775
flominatorgd <github.flominatorgd@gmail.com>
2023-05-17 18:06:50 +0200

    j7y17lte: cleanup system_prop.mk

--- system_prop.mk
+++ system_prop.mk
@@ -6,6 +6,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     audio_hal.disable_two_mic=false
 
+
 # FIFO UI scheduling
 PRODUCT_PROPERTY_OVERRIDES += \
     sys.use_fifo_ui=1
@@ -17,11 +18,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.multisim.simslotcount=1
-
-#ifneq ($(TARGET_BUILD_VARIANT),eng)
-#PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.adb.secure=1
-#endif
-
-# Display
-PRODUCT_PROPERTY_OVERRIDES += \
-    sys.display-size=1280x720
